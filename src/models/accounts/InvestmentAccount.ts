57f48561e3eca1bdff3e53d2b618e56ebcabca2d
sco1e <scole50@charlotte.edu>
2025-12-30 11:53:32 +0000

    Added the constructor, made the holdings properrty optional

--- src/models/accounts/InvestmentAccount.ts
+++ src/models/accounts/InvestmentAccount.ts
@@ -1,24 +1,21 @@
-import { Account } from "./Account";
+import { TransactionStore } from "models/transactions/TransactionStore";
+import { Account, BaseAccountParams } from "./Account";
 import { AccountType } from "./AccountType";
+
 
 export interface Holding {
     symbol: string;
     quantity: number;
     marketPrice: number;
 }
+
 export class InvestmentAccount extends Account {
-    accountType = AccountType.Investment;
-    holdings: Holding[] = [];
-
-    supportsInterest(): boolean { return false; }
-
-    calculateBalance(): number {
-        this.balance = this.holdings.reduce(
-            (sum, h) => sum + h.quantity * h.marketPrice,
-            0
-        );
-        this.balanceAsOf = new Date();
-        return this.balance;
+    holdings?: Holding[] = [];
+    constructor(params: BaseAccountParams & { holdings?: Holding[] }) {
+        super({ ...params, accountType: AccountType.Investment });
+        this.holdings = params.holdings;
     }
 
+
+    //TODO: add calcInvestmentWorth
 }
