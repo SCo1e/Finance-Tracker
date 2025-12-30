3ff55f6811492fe3f25c3e371d0b92ff37f2bea0
sco1e <scole50@charlotte.edu>
2025-12-30 13:28:01 +0000

    outlined account storage data.

--- src/utilites/storageHandlers/AccountStorage.ts
+++ src/utilites/storageHandlers/AccountStorage.ts
@@ -1,5 +1,42 @@
 import { AccountType } from "models/accounts/AccountType"
 
+// Meta info about account--> helps select which sub-class to instantiate.
+export interface AccountMeta {
+    kind: AccountType;    // Which subclass to instantiate
+    version: number;      // Schema version
+    createdAt: string;    // ISO timestamp
+    updatedAt: string;    // ISO timestamp
+}
+
+// this is subclass level details specific to each particular account type. 
+export interface AccountDetails {
+    //CheckingAccount-specific
+    overdraftLimit?: number;
+
+    // CreditCard-specific
+    creditLimit?: number;
+    apr?: number;
+    dueDay?: number;
+
+    // Savings-specific
+    interestRate?: number;
+    withdrawalLimit?: number;
+
+    // Loan-specific
+    principalAmount?: number;
+    interestRateLoan?: number;
+    termMonths?: number;
+
+    // Investment-specific
+    //holdings?:Holding[]
+    //portfolioValue?: number;
+
+    // Retirement-specific
+    //contributionRate?: number;
+    planType?: string; // e.g., 401k, IRA
+}
+
+// What is actually stored. 
 export interface AccountRecord {
     id: string;
     name: string;
@@ -12,5 +49,6 @@ export interface AccountRecord {
     transactionIds: string[];
     recurringEventIds: string[];
     //Subclass specific info.
-    meta?: Record<string | unknown>;
+    meta: AccountMeta;
+    details?: AccountDetails;
 }
